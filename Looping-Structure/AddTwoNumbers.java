28ed76ddfedec05692a40258f88555a9a9e07e70
Banuja <36789187+Banuja@users.noreply.github.com>
2018-02-24 16:23:39 +0530

    new loop

---
+++ Looping-Structure/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers{
+	public static void main (String args[]) {
+		int num1 = 22;
+		int num2 = 29;
+		int Result = num1 + num2;
+			//System.out.println(Result);
+			System.out.println("Sum of" + num1 + "+"  + num2 + "=" + Result);
+	}
+}
